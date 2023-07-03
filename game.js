16e65c70a2f91552215109a8d234ab57765451e0
hurik zamith <hurikzamith@gmail.com>
2023-07-03 09:13:32 -0300

    STEP 4: Complete

--- game.js
+++ game.js
@@ -5,23 +5,103 @@ var red = new Audio("sounds/red.mp3");
 var yellow = new Audio("sounds/yellow.mp3");
 var blue = new Audio("sounds/blue.mp3");
 var wrong = new Audio("sounds/wrong.mp3");
-
-$(document).keydown(function(event){
-  var keyPressed = event.key;
-  if(keyPressed == 's')
-    startGame();
-    else {
-      $("h1").text("Press 's'!");
-      wrongButton();
-    }
-  });
-
-// TODO: CRIAR animação do ERRO;
-// TODO: Refatorar
-// TODO: Logica do jogo
+var colors = ['green','red', 'yellow', 'blue'];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
 
 
-// Lógica de animação
+$( ".btn" ).on( "click", function() {
+  var userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+} );
+
+// function clickHandler(){
+//
+
+//   $("#green").click(function(){
+//       userChosenColour = 'green'
+//       greenAnimation();
+//     });
+
+//     $("#red").click(function(){
+//       userChosenColour = 'red'
+//       redAnimation();
+//     });
+
+//     $("#yellow").click(function(){
+//       userChosenColour = 'yellow'
+//       yellowAnimation();
+//     });
+
+//     $("#blue").click(function(){
+//       userChosenColour = 'blue'
+//       blueAnimation();
+//     });
+// }
+
+// $(document).keydown(function(event){
+//   var keyPressed = event.key;
+//   if (keyPressed == 's') {
+//     nextSequence();
+//     clickHandler();
+//     // $("h1").text("Level " + level);
+//     // startGame();
+//   }
+//   else {
+//     $("h1").text("Press 's'!");
+//     wrongButton();
+//   }
+// });
+
+
+function nextSequence(){
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomColor = colors[randomNumber];
+  playNextSequence(randomColor);
+  gamePattern.push(randomColor);
+  console.log(gamePattern);
+  level++;
+}
+
+function playNextSequence(randomColor){
+  switch (randomColor) {
+    case "green":
+      greenAnimation();
+      break;
+    case "red":
+      redAnimation();
+    break;
+    case "yellow":
+      yellowAnimation();
+    break;
+    case "blue":
+      blueAnimation();
+    break;
+    default:
+      break;
+  }
+}
+
+function matchAnswer(){
+  $(document).click("button", function(event){
+    event
+  })
+}
+
+
+function wrongButton(){
+  $("body").addClass('game-over');
+  wrong.play();
+
+  setTimeout(function() {
+    $("body").removeClass('game-over');
+  }, 100);
+}
+
+
+
 
 function greenAnimation(){
   green.play();
@@ -59,61 +139,4 @@ function blueAnimation(){
   }, 100);
 }
 
-$("#green").click(function(){
-  greenAnimation();
-});
 
-$("#red").click(function(){
-  redAnimation();
-});
-
-$("#yellow").click(function(){
-  yellowAnimation();
-});
-
-$("#blue").click(function(){
-  blueAnimation();
-});
-
-function wrongButton(){
-  $("body").addClass('game-over');
-  wrong.play();
-
-  setTimeout(function() {
-    $("body").removeClass('game-over');
-  }, 100);
-}
-
-// --------------------------------------------------------------------------
-
-
-
-function startGame(){
-  // execute
-  $("h1").text("Game Starting");
-}
-
-
-  // else {
-  //   wrongAnswer();
-  //   // WRONG ANSWER
-  //   // wrong sound + background color changes red for a sec
-  // }
-
-  // Será selecionado aleatoriamente uma das teclas, adicionar a sequencia modelo (que o usuario tentara acertar.)
-// Ela irá tocar um som e exibir um estilo.
-
-// AGUARDAR
-
-// Verificar em qual das teclas o usuário irá clicar.
-// Caso tenha clicado na mesma de antes. A sequência modelo, receberá o um novo aleatório.
-// A sequencia será exibida.
-
-// AGUARDAR ..
-
-// segue o jogo até o usuário errar a sequência.
-
-
-// Se não apertaram tecla, o jogo não começou
-
-// logo terei, o som da tecla, o som de erro e efeitos visuais de erro. A mensagem será alterada para um caso de erro tb.
