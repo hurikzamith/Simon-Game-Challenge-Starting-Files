42919b01201fb52232010a8f114e4dad9768e9b6
hurik zamith <hurikzamith@gmail.com>
2023-07-05 09:06:17 -0300

    step 8

--- game.js
+++ game.js
@@ -1,142 +1,83 @@
 
-// adicionar quando clico no verde/vermelho/amarelo/azul -> som respectivo.
-var green = new Audio("sounds/green.mp3");
-var red = new Audio("sounds/red.mp3");
-var yellow = new Audio("sounds/yellow.mp3");
-var blue = new Audio("sounds/blue.mp3");
-var wrong = new Audio("sounds/wrong.mp3");
-var colors = ['green','red', 'yellow', 'blue'];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = [];
+
+var started = false;
 var level = 0;
 
-
-$( ".btn" ).on( "click", function() {
-  var userChosenColour = this.id;
-  userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
-} );
-
-// function clickHandler(){
-//
-
-//   $("#green").click(function(){
-//       userChosenColour = 'green'
-//       greenAnimation();
-//     });
-
-//     $("#red").click(function(){
-//       userChosenColour = 'red'
-//       redAnimation();
-//     });
-
-//     $("#yellow").click(function(){
-//       userChosenColour = 'yellow'
-//       yellowAnimation();
-//     });
-
-//     $("#blue").click(function(){
-//       userChosenColour = 'blue'
-//       blueAnimation();
-//     });
-// }
-
-// $(document).keydown(function(event){
-//   var keyPressed = event.key;
-//   if (keyPressed == 's') {
-//     nextSequence();
-//     clickHandler();
-//     // $("h1").text("Level " + level);
-//     // startGame();
-//   }
-//   else {
-//     $("h1").text("Press 's'!");
-//     wrongButton();
-//   }
-// });
-
-
-function nextSequence(){
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomColor = colors[randomNumber];
-  playNextSequence(randomColor);
-  gamePattern.push(randomColor);
-  console.log(gamePattern);
-  level++;
-}
-
-function playNextSequence(randomColor){
-  switch (randomColor) {
-    case "green":
-      greenAnimation();
-      break;
-    case "red":
-      redAnimation();
-    break;
-    case "yellow":
-      yellowAnimation();
-    break;
-    case "blue":
-      blueAnimation();
-    break;
-    default:
-      break;
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
   }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  //2. Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
+  checkAnswer(userClickedPattern.length-1);
+});
+
+
+//1. Create a new function called checkAnswer(), it should take one input with the name currentLevel
+function checkAnswer(currentLevel) {
+
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+      if (userClickedPattern.length === gamePattern.length){
+
+        //5. Call nextSequence() after a 1000 millisecond delay.
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+
+    }
+
 }
 
-function matchAnswer(){
-  $(document).click("button", function(event){
-    event
-  })
+function nextSequence() {
+
+  //6. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
 
-
-function wrongButton(){
-  $("body").addClass('game-over');
-  wrong.play();
-
-  setTimeout(function() {
-    $("body").removeClass('game-over');
-  }, 100);
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
-
-
-
-function greenAnimation(){
-  green.play();
-  $("#green").addClass('pressed');
-
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
-    $("#green").removeClass('pressed');
+    $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
-
-function redAnimation(){
-  red.play();
-  $("#red").addClass('pressed');
-
-  setTimeout(function () {
-    $("#red").removeClass('pressed');
-  }, 100);
-}
-
-function yellowAnimation(){
-  yellow.play();
-  $("#yellow").addClass('pressed');
-
-  setTimeout(function () {
-    $("#yellow").removeClass('pressed');
-  }, 100);
-}
-
-function blueAnimation(){
-  blue.play();
-  $("#blue").addClass('pressed');
-
-  setTimeout(function () {
-    $("#blue").removeClass('pressed');
-  }, 100);
-}
-
-
